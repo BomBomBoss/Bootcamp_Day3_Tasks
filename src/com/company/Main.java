03a6e0a8be440775f67c4dc27c1d536181e65657
Vladislav Miheenkov <vladjuha13@gmail.com>
2022-06-29 11:28:56 +0300

    divided to different bookmarks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -32,37 +32,7 @@ public class Main {
         for (int i : array) result[index++] = i;
         return result;
     }
-    public static class BankAccount {
-        private double balance;
 
-        public BankAccount(double balance) {
-            this.balance = balance;
-        }
-        void deposit(double amount) {
-            if(amount > 5000) {
-                System.out.println("Transaction canceled. Max deposit - 5000 | Your deposit: " + amount);
-                return;}
-            balance = balance + amount;
-        }
-        void withdraw (double amount) {
-            if(amount>balance) {
-                System.out.println("There is not enough funds");
-                return;
-            }
-            balance = balance - amount;
-        }
-        void printBalance() {
-            System.out.println(balance);
-        }
-        void transferFrom(BankAccount oldBankAccount, double amount) {
-            if (oldBankAccount.balance < amount) {
-                System.out.printf("Transfer canceled. You are trying to transfer %.2f units," +
-                        " but only %.2f are available.\n",amount,oldBankAccount.balance);
-                return;
-            }
-            this.balance = this.balance + amount;
-            oldBankAccount.balance = oldBankAccount.balance - amount;
-        }
-    }
+
 
 }
